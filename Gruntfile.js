617e546f5e07d34a67f8b6c7b4416c909e44c140
udayraj123 <rhuge123@gmail.com>
2018-03-03 01:35:40 +0530

    grunt-assemble setup well

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,66 +5,39 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('./package.json'),
 
-    connect: {
-      dev: {
-        options: {
-          port: 8000,
-          base: './www'
-        }
-      }
-    },
-// assemble: {
-//   options: {
-//     flatten: true,
-//     assets: '<%= site.assets %>',
-
-//     // Metadata
-//     site: '<%= site %>',
-
-//     // Templates
-//     partials: '<%= site.includes %>',
-//     layoutdir: '<%= site.layouts %>',
-//     layout: '<%= site.layout %>',
-//   },
-//   docs: {
-//     src: ['templates/*.hbs'],
-//     dest: '<%= site.dest %>/'
-//   }
-// }
     assemble: {
-         options: {
-           layout: 'compile_homepage.html',// The final build file
-           layoutdir: './www/layouts/',
-           partials: './www/layouts/partials/**/*.html'
-         },
-         // button000: {
-         //    files: {'_demo/button-000/': ['button-000/index.html']},
-         //    options: {layout: 'none'}, // override task-level layout
-         // },
-         posts: {
-           files: [
-           {
-             cwd: './www/content/',
-             dest: './www/',//files that are processed they will be placed here
-             expand: true,
-             src: ['**/*.html', '!_body_contents/**/*.html'] //do not take _body_contents folder. take everything else.
-           }, 
-           {
-             cwd: './www/content/_body_contents/',
-             dest: './www/',//files that are processed they will be placed here
-             expand: true,
-             src: '**/*.html'
-           }
-           ]
-         }
-       }
-
+      options: {// global options
+        // This is just present always, idk what it does..
+        flatten: true,
+        // header.html, footer.html, etc common partials
+        partials: 'www/partials/*.html',
+        layoutdir: 'www/layouts',//the directory to find below file
+        layout: 'master_layout.html',// contains outermost covering layout. 
+        // Currently it's just {{> header }}{{> body }}{{> footer }}, feel creative!
+      },
+      compileAllPages:{
+        files:       {/*
+        The Syntax is: 
+            target     :     [sub_layouts],
+        
+        files in sublayouts array can contain different arrangements of these partials like {{> calender}}, feel super creative!
+        Currently, they just contain plain html.
+        */
+          'about.html'    :     ['www/contents/make_about.html'],
+          'blog.html'     :     ['www/contents/make_blog.html'],
+          'contact.html'  :     ['www/contents/make_contact.html'],
+          'gallery.html'  :     ['www/contents/make_gallery.html'],
+          'index.html'    :     ['www/contents/make_index.html'],
+          'resume.html'   :     ['www/contents/make_resume.html'],
+        },
+      },
+    }
   });
 
   /* load every plugin in package.json */
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-assemble');
 
   /* grunt tasks */
-  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('default', ['assemble']);
 
 };
